6f70bd3fee2114314334b27eb2809888823cbcd8
PANPAN24 <mizael.valadez.5@gmail.com>
2022-11-15 23:47:23 -0600

    Se modifico la interfaz del login

--- Assets/Scripts/SalirDelJuego.cs
+++ Assets/Scripts/SalirDelJuego.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class SalirDelJuego : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject aviso;
     public void Exit()
     {
         Application.Quit();
     }
+    public void ActivarAvisoSalida()
+    {
+        aviso.SetActive(true);
+    }
+    public void DesactivarAvisoSalida()
+    {
+        aviso.SetActive(false);
+    }
+
 }
